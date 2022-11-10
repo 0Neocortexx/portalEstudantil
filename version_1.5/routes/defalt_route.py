2b888cdd68fdb01ddb68845fa72cd56cc70e1e97
0Neocortexx <jonathanemmanueldeoliveira2003@gmail.com>
2022-11-10 10:37:20 -0300

    corrigindo ips e rota padrao

--- version_1.5/routes/defalt_route.py
+++ version_1.5/routes/defalt_route.py
@@ -2,4 +2,9 @@ from config import *
 
 @app.route("/")
 def inicio():
+    ip = request.host_url
+    return f'<p>Projeto de apoio discente, comunicação e suporte em disciplinas do ensino médio. <br>Colaboradores:  <br> Aline Scholl Santos; <br> Jonathan Emmanuel De Oliveira</p> <br> <a href="{ip}main">Ir para o site!</a>'
+
+@app.route('/main')
+def main():
     return render_template('index.html')
